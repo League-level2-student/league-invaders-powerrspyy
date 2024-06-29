498c83cb7b88f934ee87cc67cfdf2e5b6d88c174
powerrspyy <147744817+powerrspyy@users.noreply.github.com>
2024-06-28 18:33:36 -0700

    Update LeagueInvaders.java

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,28 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+    
 
 public class LeagueInvaders {
+    
+    public static void setup(){
+    GamePanel gamePanel = new GamePanel();
+    
+    final int HEIGHT = 800;
+    final int WIDTH = 300;
+
+    JFrame frame = new JFrame();
+
+    frame.setSize(WIDTH, HEIGHT);
+
+    JLabel label = new JLabel("League Invaders");
+
+    frame.add(label);
+    frame.add(gamePanel);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    frame.setVisible(true);}
+public static void main(String[] args){
+setup();
+}    
 
 }
